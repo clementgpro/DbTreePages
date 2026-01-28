232aa82c2b91d83ba0344436784fd52868c8acef
Clément <clemzd@gmail.com>
2026-01-28 18:27:35 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/DbTreePages">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/download" element={<Download />} />
