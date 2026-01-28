304d7c1c404112d62eb93abea29762ced058cb82
Clément <clemzd@gmail.com>
2026-01-28 18:24:33 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? "/DbTreePages": "/",
+  base: mode === 'production' ? "/DbTreePages/": "/",
   server: {
     host: "::",
     port: 8080,
